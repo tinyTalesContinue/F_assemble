520687cbe19b7cc84a80a5a3f9806e8a81cef4be
tinyTalesContinue <harutakumao.momo@icloud.com>
2024-12-26 21:31:06 +0900

    Add files via upload

---
+++ Operand.java
@@ -0,0 +1,24 @@
+//各種、演算するためのアセンブリコードを出力する関数
+
+public class Operand {
+
+    //足し算を実行する関数
+    public void Plus() {
+    }
+
+    //引き算を実行する関数
+    public void Minus() {
+
+    }
+
+    //掛け算を実行する関数
+    public void Mult() {
+
+    }
+
+    //割り算を実行する関数
+    public void Div() {
+
+    }
+
+}
